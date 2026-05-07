2831b8b7508f96fb8495e57403c048d0b55d6d6a
Gershom Umar <gershomumar@Gershoms-Macbook-Pro.local>
2026-05-07 16:55:22 +0100

    Fix: restore missing const app = express()

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const cors     = require('cors');
 const path     = require('path');
 const fs       = require('fs');
 
+const app  = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
